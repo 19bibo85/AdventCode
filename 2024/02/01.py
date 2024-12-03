cc0af5932a19aa52c3a1ee845b78a9e7c80e34e7
85bibo85 <alberto.tosibrandi@gmail.com>
2024-12-03 09:01:25 +0000

    add day 2

---
+++ 2024/02/01.py
@@ -0,0 +1,24 @@
+c = 0
+
+with open("2024\\02\\files\\01.txt") as f:
+    while(True):
+        nums = list(map(int, f.readline().split()))
+        if(len(nums) <= 0):
+            break
+
+        i = 0
+        v = 1
+        while(i < len(nums) - 1):
+            if (nums[0] < nums[1]):
+                if (not(nums[i+1] >= nums[i] + 1 and nums[i+1] <= nums[i] + 3)):
+                    v = 0
+                    break
+            else:                
+                if (not(nums[i+1] >= nums[i] - 3 and nums[i+1] <= nums[i] - 1)):
+                    v = 0
+                    break
+            i += 1
+        c += v
+
+print(c)
+        
